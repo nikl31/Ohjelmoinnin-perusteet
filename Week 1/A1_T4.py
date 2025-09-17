1d8adcb6867e8365f6582591bb1daa8d2eaf7dbd
nikl31 <niklas.kotilainen@student.lab.fi>
2025-09-17 18:00:19 +0000

    tehtävä 4

--- Week 1/A1_T4.py
+++ Week 1/A1_T4.py
@@ -1,1 +1,10 @@
+Num1 = 47
+Num2 = 102
+sum = Num1 + Num2
+diff = Num2 - Num1
+product = sum * diff
+print(f"{Num1} + {Num2} = {sum}")
+print(f"{Num2} - {Num1} = {diff}")
+print(f"{sum} * {diff} = {product}")
+print(f"({Num1} + {Num2}) * ({Num2} - {Num1}) = {product}")
 
