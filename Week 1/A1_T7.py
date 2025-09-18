36f39d7c1a589b923970ad977da7ca56a0149b3a
nikl31 <niklas.kotilainen@student.lab.fi>
2025-09-18 10:19:59 +0000

    Tehtävä 7

--- Week 1/A1_T7.py
+++ Week 1/A1_T7.py
@@ -1,1 +1,13 @@
+print("Calculate fuel consumption.")
+Feed = input("Enter travel distance(kilometer):" )
+Distance = int(Feed) 
+input("Enter fuel usage (liters):" )
+FuelUsage = int(Feed)
+Consumption = 200/100
+Consumption = int(Consumption)
+print(f"Fuel consumption is {Consumption} l per 100 km")
 
+# CALCULATE FUEL CONSUMPTION
+# ENTER TRAVEL DISTANCE (KILOMETERS): 200
+# ENTER FUEL USAGE (LITERS): 20
+# FUEL CONSUMPTION IS 10 L PER 100 KM
