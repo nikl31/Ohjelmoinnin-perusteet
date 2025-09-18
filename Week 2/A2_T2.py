ac41168d0ad0dd163ca3b9e01e26f4c69118b99c
nikl31 <niklas.kotilainen@student.lab.fi>
2025-09-18 10:57:40 +0000

    Tehtävä 2

--- Week 2/A2_T2.py
+++ Week 2/A2_T2.py
@@ -1,1 +1,7 @@
+print("Program starting")
+Brand = input("Insert car brand: ")
+Model = input("Insert car model: ")
+print(f"Car brand is {Brand}. ") 
+print(f"and the model is {Model}.")
+print("Program ending")
 
