dc048b46a5e4b295933c6abd90a28b3ec6801228
nikl31 <niklas.kotilainen@student.lab.fi>
2025-09-18 11:28:42 +0000

    Tehtävä 3

--- Week 2/A2_T3.py
+++ Week 2/A2_T3.py
@@ -1,1 +1,9 @@
+print("Program starting")
+word1 = input("Insert first word: ")  
+word2 = input("Insert second word: ")
+print(f"word1 is {len(word1)} characters long.")
+print(f"word2 is {len(word2)} characters long.")
+print("Words together make one closed compound'"+ word1 + word2 +"',")
+print("Program ending")
+
 
