4140d51c3f5beb57c64961c2aa01491057386795
nikl31 <niklas.kotilainen@student.lab.fi>
2025-09-18 12:16:32 +0000

    Tehtävä 5

--- Week 1/A1_T5.py
+++ Week 1/A1_T5.py
@@ -1,1 +1,9 @@
+print("Calculate the are of a wall.")
+Feed = input("Enter the width in meters: ")
+Width = int(Feed)
+Feed = input("Enter rhe height in meters: ")
+Height = int(Feed)
+print("Width is", Width, "m and height is", Height, "m.")
+Area = Width * Height
+print("The wall will be", Area,"Square meters.")    
 
