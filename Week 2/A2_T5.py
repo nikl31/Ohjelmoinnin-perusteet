44c0ac2163cf68e93c8e5bd8712c07a0c1232d1e
nikl31 <niklas.kotilainen@student.lab.fi>
2025-09-22 12:32:50 +0000

    Tehtävä 5

--- Week 2/A2_T5.py
+++ Week 2/A2_T5.py
@@ -1,3 +1,15 @@
 print("Program starting.")
 task1 =input("Insert a closed compound word: ")
-print(f"The word you inserted is {task1}")
+reverse = task1[::-1]
+print(f"The word you inserted is '{task1}' and in reverse it is '{reverse}'.")
+length = (len(task1))
+print(f"the inserted word length is {length}")
+last = task1[-1]
+print(f"Last character is '{last}'")
+print("Take substring from the inserted word by inserting...")
+first = int(input("1) Starting point: "))
+second = int(input("2) Ending point: "))
+third = int(input("3) Step size: "))
+complete = task1[first:second:third]
+print(f"The word '{task1}' sliced to the defined substring is '{complete}'.")
+print("Program ending.")
