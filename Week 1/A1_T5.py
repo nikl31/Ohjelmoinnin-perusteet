c2e61b744acaf5b15eb3e56045d448264a3e146a
nikl31 <niklas.kotilainen@student.lab.fi>
2025-09-22 12:38:16 +0000

    Tehtävä 5

--- Week 1/A1_T5.py
+++ Week 1/A1_T5.py
@@ -1,7 +1,7 @@
 print("Calculate the area of a wall.")
 Feed = input("Enter the width in meters: ")
 Width = int(Feed)
-Feed = input("Enter rhe height in meters: ")
+Feed = input("Enter the height in meters: ")
 Height = int(Feed)
 print("Width is", Width, "m and height is", Height, "m.")
 Area = Width * Height
