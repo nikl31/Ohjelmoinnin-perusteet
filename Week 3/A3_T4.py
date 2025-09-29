f8e73ec17fbd8e70260e7a038c63da28fffe573a
nikl31 <niklas.kotilainen@student.lab.fi>
2025-09-29 17:31:36 +0300

    Tehtävä 4

--- Week 3/A3_T4.py
+++ Week 3/A3_T4.py
@@ -1,6 +1,6 @@
 print("Program starting.")
 print("This is a program with simple menu where you can choose which operation the program performs.")
-name1 = input("Before the menu, please insert your name: ")
+Name = input("Before the menu, please insert your name: ")
 print("")
 print("Options:")
 print("1 - Print welcome message")
@@ -8,7 +8,13 @@ print("2 - Print the name backwards")
 print("3 - Print the first character")
 print("4 - Show the amount of characters in the name")
 print("0 - Exit")
-choice = input("Your choice: ")
-print ("Welcome", name1)
-print("Program ending.")
+choice = int(input("Your choice: "))
+
+if (choice == 1):
+    print (f"Welcome {Name}!")
+elif(choice == 2):
+    print("Your name backwards is {NameBackwards}")
+
+
+
 
