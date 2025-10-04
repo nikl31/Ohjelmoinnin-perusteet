3ab693eb5f8716552583bc87ea62c87516d382f5
nikl31 <niklas.kotilainen@student.lab.fi>
2025-10-04 22:42:09 +0300

    Tehtävä 1

--- Week 4/A4_T1.py
+++ Week 4/A4_T1.py
@@ -5,3 +5,13 @@ print("\nStarting for-loop:")
 for i in range(start, stop + 1):
     print(i)
 print("\nProgram ending.")
+print("Program starting.\n")
+
+start = int(input("Insert starting value: "))
+stop = int(input("Insert stopping value: "))
+
+print("\nStarting for-loop:")
+for i in range(start, stop + 1):
+    print(i)
+
+print("\nProgram ending.")
