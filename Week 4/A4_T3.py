c28678442952fc7dd047e666c0ee90e841124ad6
nikl31 <niklas.kotilainen@student.lab.fi>
2025-10-04 22:44:09 +0300

    Tehtävä 3

--- Week 4/A4_T3.py
+++ Week 4/A4_T3.py
@@ -1,1 +1,16 @@
+print("Program starting.\n")
+
+start = int(input("Insert starting value: "))
+stop = int(input("Insert stopping value: "))
+
+print("\nStarting while-loop:")
+i = start
+while i <= stop:
+    if i < stop:
+        print(i, end=" ")
+    else:
+        print(i)
+    i += 1
+
+print("\nProgram ending.")
 
