5c536751f43311adf9fa5515eb27457013fbabd0
nikl31 <niklas.kotilainen@student.lab.fi>
2025-10-04 22:52:22 +0300

    Tehtävä 7

--- Week 4/A4_T7.py
+++ Week 4/A4_T7.py
@@ -1,1 +1,19 @@
+print("Program starting.\n")
 
+print("Check multiplicative persistence.")
+n = input("Insert an integer: ")
+
+steps = 0
+
+while len(n) > 1:
+    digits = [int(d) for d in n]
+    product = 1
+    for d in digits:
+        product *= d
+    print(" * ".join(n), "=", product)
+    n = str(product)
+    steps += 1
+
+print("No more steps.\n")
+print(f"This program took {steps} step(s)\n")
+print("Program ending.")
