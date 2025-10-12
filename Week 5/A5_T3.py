8fcae5ff8ae000a8194accccbfae2ada21fc6c9a
nikl31 <niklas.kotilainen@student.lab.fi>
2025-10-12 21:57:45 +0300

    Tehtävä 3

--- Week 5/A5_T3.py
+++ Week 5/A5_T3.py
@@ -1,1 +1,15 @@
+def askName():
+    name = input("Insert name: ")
+    return name
 
+def greetUser(PName):
+    print(f"Hello {PName}!")
+
+def main():
+    print("Program starting.")
+    name = askName()
+    greetUser(name)
+    print("Program ending.")
+
+if __name__ == "__main__":
+    main()
