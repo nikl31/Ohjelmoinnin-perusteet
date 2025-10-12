07adce6916da616598df2c4676fcddf41648a76b
nikl31 <niklas.kotilainen@student.lab.fi>
2025-10-12 21:57:13 +0300

    Tehtävä 2

--- Week 5/A5_T2.py
+++ Week 5/A5_T2.py
@@ -1,1 +1,20 @@
+def frameWord(PWord):
+    frame_length = len(PWord) + 4
+    print('*' * frame_length)
+    print(f"* {PWord} *")
+    print('*' * frame_length)
 
+
+def main():
+    print("Program starting.")
+    print()
+    PWord = input("Insert word: ")
+    print()
+    frameWord(PWord)
+    print()
+    print("Program ending.")
+
+
+if __name__ == "__main__":
+    main()
+    
