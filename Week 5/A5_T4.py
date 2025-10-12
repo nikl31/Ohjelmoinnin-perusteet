2e27e79dca2e4f145782fc9afebe0c1a8a5ddd0a
nikl31 <niklas.kotilainen@student.lab.fi>
2025-10-12 20:41:15 +0300

    Tehtävä 4

--- Week 5/A5_T4.py
+++ Week 5/A5_T4.py
@@ -1,1 +1,19 @@
+def askDimension(PPrompt: str) -> float:
+    Feed = input(f"Insert {PPrompt}: ")
+    return float(Feed)
+
+
+def calcRectangleArea(PWidth: float, PHeight: float) -> float:
+    Area = PWidth * PHeight
+    return Area
+
+
+print("Program starting.")
+Width = askDimension("width")
+Height = askDimension("height")
+
+Area = calcRectangleArea(Width, Height)
+print()
+print(f"Area is {Area}²")
+print("Program ending.")
 
