6588a953ae08724610897e6460150b41d4685e1e
nikl31 <niklas.kotilainen@student.lab.fi>
2025-10-20 20:47:20 +0300

    Tehtävä 1

--- Week 6/A6_T1.py
+++ Week 6/A6_T1.py
@@ -1,1 +1,15 @@
+def read(filename):
+ 
+print("Program starting.")
+print("This program can read a file.")
+filename = input("Insert filename: ")
 
+ file = open(filename, 'r', encoding="utf-8")
+ while True:
+   line = file.readline()
+   if len(line) == 0:
+    break
+   line = line.strip()
+   parts = line.split(';')
+   print(f"Name: {parts[0]}, Height: {parts[1]}")
+ file.close()
