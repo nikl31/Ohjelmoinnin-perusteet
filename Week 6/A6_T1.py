feb1cdcb0a04c9095fe1466ef5655b0fd034d309
nikl31 <niklas.kotilainen@student.lab.fi>
2025-10-26 22:11:45 +0200

    Tehtävä 1

--- Week 6/A6_T1.py
+++ Week 6/A6_T1.py
@@ -1,15 +1,18 @@
-def read(filename):
- 
-print("Program starting.")
-print("This program can read a file.")
-filename = input("Insert filename: ")
+def main():
+    print("Program starting.")
+    print("This program can read a file.")
+    filename = input("Insert filename: ")
 
- file = open(filename, 'r', encoding="utf-8")
- while True:
-   line = file.readline()
-   if len(line) == 0:
-    break
-   line = line.strip()
-   parts = line.split(';')
-   print(f"Name: {parts[0]}, Height: {parts[1]}")
- file.close()
+    try:
+        with open(filename, "r", encoding="utf-8") as file:
+            print(f'#### START "{filename}" ####')
+            print(file.read(), end="")  # print file content without extra newlines
+            print(f'\n#### END "{filename}" ####')
+    except FileNotFoundError:
+        print(f'Error: File "{filename}" not found.')
+
+    print("Program ending.")
+
+
+if __name__ == "__main__":
+    main()
