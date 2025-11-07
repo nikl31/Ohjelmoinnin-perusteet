f61e3b2f887cf301136f01a1af0277fe18a8a094
nikl31 <niklas.kotilainen@student.lab.fi>
2025-11-07 14:43:57 +0200

    Tehtävä 7

--- Week 6/A6_T7.py
+++ Week 6/A6_T7.py
@@ -1,1 +1,31 @@
+LOWER_ALPHABETS = "abcdefghijklmnopqrstuvwxyz"
+UPPER_ALPHABETS = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
 
+def readFile(PFilename) -> str:
+    Content = ''
+    FileHandle = open(PFilename, 'r')
+    Row = FileHandle.readline()
+    while Row != '':
+        Content += Row
+        Row = FileHandle.readline()
+    FileHandle.close()
+    return Content
+
+def writeFile() -> None:
+    return None
+
+def appendToFile() -> None:
+    return None
+
+def rot() -> str:
+    newContent = ''
+    return newContent
+
+def main() -> None:
+    print("Travel starting.")
+    PlayerProgressFilename = "player_progress.txt"
+    Progress = readFile(PlayerProgressFilename)
+    print(Progress)
+    return None
+
+main()
