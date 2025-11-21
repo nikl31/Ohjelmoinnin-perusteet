6faa783647aa71bec777fa47b6cd09624af83698
nikl31 <niklas.kotilainen@student.lab.fi>
2025-11-21 18:00:08 +0200

    Tehtävä 2

--- Week 6/A6_T2.py
+++ Week 6/A6_T2.py
@@ -1,1 +1,20 @@
+def writeFile(PFilename, PContent) -> None:
+    Filehandle = open(PFilename, 'w')
+    Filehandle.write(PContent)
+    Filehandle.close()
+    return None
+
+
+def main() -> None:
+    print("Program starting")
+    FirstName = input("Insert first name: ")
+    LastName = input("Insert your last name: ")
+    Filename = input("Insert filename: ")
+    Content = "{}\n{}\n".format(FirstName, LastName)
+    writeFile(Filename, Content)
+    print("Program ending")
+    return None
+
+if __name__ == "__main__":
+    main()
 
