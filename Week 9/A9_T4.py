6dee88b56ed69e395f125e4651660d52fedde5e5
nikl31 <niklas.kotilainen@student.lab.fi>
2025-11-23 21:33:24 +0200

    Tehtävä 4

--- Week 9/A9_T4.py
+++ Week 9/A9_T4.py
@@ -1,1 +1,32 @@
+########################################################
+# Task A9_T4
+# Developer First_name Last_name
+# Date 2025-11-23
+########################################################
 
+TEMP_MIN = -273.15
+TEMP_MAX = 10000
+
+def collectCelsius() -> float:
+    feed = input("Insert Celsius: ")
+    try:
+        celsius = float(feed)
+    except ValueError:
+        raise ValueError(f"could not convert string to float: '{feed}'")
+    
+    if not (TEMP_MIN <= celsius <= TEMP_MAX):
+        raise Exception(f"{celsius} temperature out of range.")
+    
+    return celsius
+
+def main():
+    print("Program starting.")
+    try:
+        celsius = collectCelsius()
+        print(f"You inserted {celsius} °C")
+    except Exception as e:
+        print(f"Error! {e}")
+    print("Program ending.")
+
+if __name__ == "__main__":
+    main()
