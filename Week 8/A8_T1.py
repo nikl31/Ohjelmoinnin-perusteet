544b86b1be9764777123f4573dc2293c8221e048
nikl31 <niklas.kotilainen@student.lab.fi>
2025-11-23 21:17:47 +0200

    Tehtävä 1

--- Week 8/A8_T1.py
+++ Week 8/A8_T1.py
@@ -1,1 +1,38 @@
+import time
 
+def main():
+    print("Program starting.")
+    pause_duration = 0.0
+
+    while True:
+        print("Options:")
+        print("1 - Set pause duration")
+        print("2 - Activate pause")
+        print("0 - Exit")
+        choice = input("Your choice: ")
+
+        if choice == "1":
+            value = input("Insert pause duration (s): ")
+            try:
+                pause_duration = float(value)
+            except ValueError:
+                print("Invalid value, pause duration unchanged.\n")
+                continue
+            print()
+
+        elif choice == "2":
+            print(f"Pausing for {pause_duration} seconds.")
+            time.sleep(pause_duration)
+            print("Unpaused.\n")
+
+        elif choice == "0":
+            print("Exiting program.\n")
+            break
+
+        else:
+            print("Invalid choice.\n")
+
+    print("Program ending.")
+
+if __name__ == "__main__":
+    main()
